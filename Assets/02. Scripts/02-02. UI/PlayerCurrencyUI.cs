a79d3491c2c24cec576c75ff80b70febbce799fd
KimNayeon <gameya120@gmail.com>
2026-06-19 17:18:22 +0900

    ADD UI
    1. Life UI Panel 추가
    2. Gold UI Panel 추가
    3. Turret 프리펩 Sorting Layer 수정
    4. Turret의 Ghost 프리펩 추가

---
+++ Assets/02. Scripts/02-02. UI/PlayerCurrencyUI.cs
@@ -0,0 +1,18 @@
+using TMPro;
+using UnityEngine;
+
+public class PlayerCurrencyUI : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI[] textCurrency;
+    /*
+    [SerializeField] 소지 재화 확인 후 수정 예정
+    private
+
+
+    private void Update()
+    {
+        textCurrency.text =
+    }
+    */
+}

8d07596e8493b56df9ad4d8c79bb39a80c11f76e
KimNayeon <gameya120@gmail.com>
2026-06-30 13:58:48 +0900

    Update MainSceneController.cs

--- Assets/02. Scripts/02-04. GameFlow/MainScene/MainSceneController.cs
+++ Assets/02. Scripts/02-04. GameFlow/MainScene/MainSceneController.cs
@@ -20,6 +20,15 @@ public class MainSceneController : MonoBehaviour
             _settingButton.onClick.RemoveAllListeners();
             _settingButton.onClick.AddListener(UI_Manager.Instance.OpenSettingWindow);
         }
+        
+        if (CurrencyManager.Instance != null)
+        {
+            CurrencyManager.Instance.InitGold(100);
+            CurrencyManager.Instance.InitElementOrbs(EElement.Water, 0);
+            CurrencyManager.Instance.InitElementOrbs(EElement.Fire, 0);
+            CurrencyManager.Instance.InitElementOrbs(EElement.Grass, 0);
+            CurrencyManager.Instance.InitElementOrbs(EElement.Electric, 0);
+        }
     }
 
     private void Update()
